7550188001aa461b2ce6996eae7c26174d42aa96
magicandskill <155175649+magicandskill@users.noreply.github.com>
2023-12-29 11:26:50 -0500

    Create ignored.py

---
+++ ignored.py
@@ -0,0 +1,3 @@
+words = ['the','she','he','her','his','herself','himself','it','i','me','i\'ll','you','and','to','is','was','are','were',
+'a','an','of','i','but','on','in','at','had','have','has','or','for','into','what','why','how','where','which','that'
+,'this','not','so','with','as','do','be']
